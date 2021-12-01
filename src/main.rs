5f41df80faf0bc07b9053d6d62d203bf911161ca
Refsa <refsa22@gmail.com>
2021-12-01 12:12:44 +0100

    some minor utility stuff

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,66 @@
 use crate::runner::Runner;
 
-mod runner;
 mod aoc1;
+mod runner;
 
 fn get_runner(day: usize) -> impl Runner {
     match day {
-        1 => aoc1::AOC1{},
+        1 => aoc1::AOC1 {},
         _ => panic!("Runner for day {} not implemented", day),
     }
 }
 
 fn main() {
-    let day = 1;
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 1 {
+        panic!("expected first argument to be day as number");
+    }
+    let day = match args[1].parse::<usize>() {
+        Ok(day) => day,
+        Err(_) => panic!("expected first argument to be a number"),
+    };
 
     let runner = get_runner(day);
 
-    let test_data = get_test_data(day);
-    let test_answer = runner.test(&test_data.input);
+    // Run test input
+    {
+        let test_data = get_test_data(day);
+        let test_answer = runner.test(&test_data.input);
 
-    if test_answer != test_data.answer {
-        println!("Failed test: expected {} - got answer {}", test_data.answer, test_answer);
-        return;
+        if test_answer != test_data.answer {
+            println!(
+                "Failed test: expected {} - got answer {}",
+                test_data.answer, test_answer
+            );
+            return;
+        }
+        println!("Test success");
     }
-    println!("Test success");
+    // Run puzzle input
+    {
+        let input = get_input(day);
+        println!("Part 1: {}", runner.run_p1(&input));
+        println!("Part 2: {}", runner.run_p2(&input));
+    }
+}
 
-    let input = get_input(day);
-    println!("Part 1: {}", runner.run_p1(&input));
-    println!("Part 2: {}", runner.run_p2(&input));
+fn read_file(path: String) -> Result<Vec<String>, String> {
+    let pb = std::path::PathBuf::from(&path);
+    if !pb.exists() {
+        return Err(format!("Couldnt find file at path: {}", path));
+    }
+    let content = std::fs::read_to_string(pb).unwrap();
+    Ok(content.lines().map(|e| e.to_string()).collect())
 }
 
 fn get_input(day: usize) -> Vec<String> {
     let path = format!("./resources/day{}.txt", day);
-    let content = std::fs::read_to_string(path).unwrap();
-    return content.lines().map(|e| e.to_string()).collect();
+    let content = read_file(path);
+
+    match content {
+        Ok(content) => content,
+        Err(_) => panic!("Couldnt find puzzle input for day {}", day),
+    }
 }
 
 struct TestData {
@@ -43,16 +71,18 @@ impl TestData {
     pub fn new(input: Vec<String>, answer: usize) -> Self {
         Self {
             input: input,
-            answer: answer
+            answer: answer,
         }
     }
 }
 
 fn get_test_data(day: usize) -> TestData {
     let path = format!("./resources/day{}_test.txt", day);
-    let content = std::fs::read_to_string(path).unwrap();
-    let lines: Vec<&str> = content.lines().collect();
-    
+    let lines = match read_file(path) {
+        Err(_) => panic!("Couldnt find test input for day {}", day),
+        Ok(lines) => lines,
+    };
+
     let answer: usize = lines[0].parse::<usize>().unwrap();
     let input: Vec<String> = lines.iter().skip(2).map(|e| e.to_string()).collect();
 
