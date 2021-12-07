fa1f8c31d2c09e59c5107e845be715b63ddc083b
Refsa <refsa22@gmail.com>
2021-12-07 19:11:47 +0100

    changed aoc6 to use rotate_left instead of manual approach

--- src/aoc6.rs
+++ src/aoc6.rs
@@ -16,44 +16,32 @@ impl Runner for AOC6 {
 
     fn run_p1(&self) -> usize {
         let mut buckets = [0usize; 9];
-        let mut temp = [0usize; 9];
 
         for p in &self.parsed {
             buckets[*p as usize] += 1;
         }
 
         for i in 0..80 {
-            for j in 1..buckets.len() {
-                temp[j - 1] = buckets[j];
-            }
-            temp[6] += buckets[0];
-            temp[8] = buckets[0];
-
-            for j in 0..9 {
-                buckets[j] = temp[j];
-            }
+            let fst = buckets[0];
+            buckets.rotate_left(1);
+            buckets[6] += fst;
+            buckets[8] = fst;
         }
 
         buckets.iter().sum::<usize>() as usize
     } 
     fn run_p2(&self) -> usize {
         let mut buckets = [0usize; 9];
-        let mut temp = [0usize; 9];
 
         for p in &self.parsed {
             buckets[*p as usize] += 1;
         }
 
         for i in 0..256 {
-            for j in 1..buckets.len() {
-                temp[j - 1] = buckets[j];
-            }
-            temp[6] += buckets[0];
-            temp[8] = buckets[0];
-
-            for j in 0..9 {
-                buckets[j] = temp[j];
-            }
+            let fst = buckets[0];
+            buckets.rotate_left(1);
+            buckets[6] += fst;
+            buckets[8] = fst;
         }
 
         buckets.iter().sum::<usize>() as usize
