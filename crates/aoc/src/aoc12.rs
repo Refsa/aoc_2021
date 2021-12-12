a726db2af231e17686862cbad61bde88e5dc1d38
Refsa <refsa22@gmail.com>
2021-12-12 10:33:53 +0100

    cleanup of aoc12 bench for aoc12

--- crates/aoc/src/aoc12.rs
+++ crates/aoc/src/aoc12.rs
@@ -1,26 +1,22 @@
 use crate::runner::Runner;
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::rc::Rc;
 
 type NodeIndex = usize;
 type EdgeIndex = usize;
-type EdgeCost = usize;
 
 #[derive(Debug)]
 enum Node {
-    Entrance(NodeIndex, String),
-    Exit(NodeIndex, String),
-    SmallCave(NodeIndex, String),
-    BigCave(NodeIndex, String),
+    Entrance(NodeIndex),
+    Exit(NodeIndex),
+    SmallCave(NodeIndex),
+    BigCave(NodeIndex),
 }
 
 #[derive(Default)]
 struct Edge {
     node_a: NodeIndex,
     node_b: NodeIndex,
-    edge_cost: EdgeCost,
 }
 
 #[derive(Default)]
@@ -39,16 +35,15 @@ impl Graph {
         let node = match ident {
             "start" => {
                 self.entrance = idx;
-                Node::Entrance(idx, ident.to_string())
+                Node::Entrance(idx)
             }
             "end" => {
                 self.exit = idx;
-                Node::Exit(idx, ident.to_string())
+                Node::Exit(idx)
             }
-            _ if (ident.chars().nth(0).unwrap() as u8) < 97 => {
-                Node::BigCave(idx, ident.to_string())
-            }
-            _ => Node::SmallCave(idx, ident.to_string()),
+            // find upper case based on ASCII index
+            _ if (ident.chars().nth(0).unwrap() as u8) < 97 => Node::BigCave(idx),
+            _ => Node::SmallCave(idx),
         };
 
         self.nodes.push(node);
@@ -65,7 +60,6 @@ impl Graph {
         let edge = Edge {
             node_a: idx_a,
             node_b: idx_b,
-            edge_cost: 0,
         };
         let edge_idx = self.edges.len();
 
@@ -117,40 +111,32 @@ impl Runner for AOC12 {
 
             graph.add_edge(a_idx, b_idx);
         }
-        /* println!();
-        for (name, node) in &graph.node_lookup {
-            println!("{} : {:?}", name, graph.nodes[*node]);
-        }
-
-        println!();
-        for edge in &graph.edges {
-            println!("{:?} - {:?}", graph.nodes[edge.node_a], graph.nodes[edge.node_b]);
-        }
-
-        println!();
-        for (nidx, eidxs) in &graph.edge_lookup {
-            println!("{:?} - {:?}", graph.nodes[*nidx], eidxs);
-        } */
 
         self.graph = graph;
     }
     fn run_p1(&self) -> usize {
-        find_paths_p1(&self.graph).len()
+        let mut paths = Vec::new();
+
+        find_paths_p1(
+            &self.graph,
+            self.graph.entrance,
+            HashSet::new(),
+            vec![],
+            &mut paths,
+        );
+
+        paths.len()
     }
     fn run_p2(&self) -> usize {
-        find_paths_p2(&self.graph).len()
+        let mut paths = Vec::new();
+
+        find_paths_p2(&self.graph, self.graph.entrance, 0, HashSet::new(), vec![], &mut paths);
+
+        paths.len()
     }
 }
 
-fn find_paths_p1(graph: &Graph) -> Vec<Vec<NodeIndex>> {
-    let mut paths = Vec::new();
-
-    find_path_p1(graph, graph.entrance, HashSet::new(), vec![], &mut paths);
-
-    return paths;
-}
-
-fn find_path_p1(
+fn find_paths_p1(
     graph: &Graph,
     current: NodeIndex,
     mut visited: HashSet<NodeIndex>,
@@ -160,15 +146,14 @@ fn find_path_p1(
     path.push(current);
 
     match &graph.nodes[current] {
-        Node::Entrance(_, _) => (),
-        Node::Exit(_, _) => {
+        Node::Exit(_) => {
             paths.push(path);
             return;
         }
-        Node::SmallCave(_, _) => {
+        Node::SmallCave(_) => {
             let _ = visited.insert(current);
         }
-        Node::BigCave(_, _) => (),
+        _ => (),
     }
 
     for &n in graph.edge_lookup.get(&current).unwrap() {
@@ -183,19 +168,11 @@ fn find_path_p1(
             continue;
         }
 
-        find_path_p1(graph, target, visited.clone(), path.clone(), paths);
+        find_paths_p1(graph, target, visited.clone(), path.clone(), paths);
     }
 }
 
-fn find_paths_p2(graph: &Graph) -> Vec<Vec<NodeIndex>> {
-    let mut paths = Vec::new();
-
-    find_path_p2(graph, graph.entrance, 0, HashSet::new(), vec![], &mut paths);
-
-    return paths;
-}
-
-fn find_path_p2(
+fn find_paths_p2(
     graph: &Graph,
     current: NodeIndex,
     mut small_visit_count: u8,
@@ -206,17 +183,16 @@ fn find_path_p2(
     path.push(current);
 
     match &graph.nodes[current] {
-        Node::Entrance(_, _) => (),
-        Node::Exit(_, _) => {
+        Node::Exit(_) => {
             paths.push(path);
             return;
         }
-        Node::SmallCave(_, _) => {
+        Node::SmallCave(_) => {
             if !visited.insert(current) {
                 small_visit_count += 1;
             }
         }
-        Node::BigCave(_, _) => (),
+        _ => (),
     }
 
     for &n in graph.edge_lookup.get(&current).unwrap() {
@@ -231,6 +207,13 @@ fn find_path_p2(
             continue;
         }
 
-        find_path_p2(graph, target, small_visit_count, visited.clone(), path.clone(), paths);
+        find_paths_p2(
+            graph,
+            target,
+            small_visit_count,
+            visited.clone(),
+            path.clone(),
+            paths,
+        );
     }
 }
