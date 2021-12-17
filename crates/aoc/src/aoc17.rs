405d0e87bac6e5af24e080855064a0e07f68e9fd
Refsa <refsa22@gmail.com>
2021-12-17 14:07:20 +0100

    more aoc17 cleanup

--- crates/aoc/src/aoc17.rs
+++ crates/aoc/src/aoc17.rs
@@ -2,17 +2,10 @@ use std::ops::{Add, RangeInclusive, Sub};
 
 use crate::runner::Runner;
 
+#[derive(Default)]
 pub struct AOC17 {
-    x_range: RangeInclusive<isize>,
-    y_range: RangeInclusive<isize>,
-}
-impl Default for AOC17 {
-    fn default() -> Self {
-        Self {
-            x_range: (0..=0),
-            y_range: (0..=0),
-        }
-    }
+    x_range: (isize, isize),
+    y_range: (isize, isize),
 }
 
 impl Runner for AOC17 {
@@ -23,15 +16,11 @@ impl Runner for AOC17 {
         let (x1, x2) = &x[2..].split_once("..").unwrap();
         let (y1, y2) = &y[3..].split_once("..").unwrap();
 
-        let x = (x1.parse::<isize>().unwrap(), x2.parse::<isize>().unwrap());
-        let y = (y1.parse::<isize>().unwrap(), y2.parse::<isize>().unwrap());
-
-        self.x_range = x.0..=x.1;
-        self.y_range = y.0..=y.1;
+        self.x_range = (x1.parse::<isize>().unwrap(), x2.parse::<isize>().unwrap());
+        self.y_range = (y1.parse::<isize>().unwrap(), y2.parse::<isize>().unwrap());
     }
 
     fn run_p1(&self) -> usize {
-        let target_area: TargetArea = (self.x_range.clone(), self.y_range.clone()).into();
         /*
         the maximum height we can go before the velocity when reaching y = 0
         is equal to the lower y bounds of the target area
@@ -43,19 +32,21 @@ impl Runner for AOC17 {
         the x velocity doesnt matter since we can just assume it's the lowest
         amount for the x velocity to stop at the lower x bound of the target area
         */
-        sum(-target_area.min_y - 1) as usize
+        sum(-self.y_range.0 - 1) as usize
     }
 
     fn run_p2(&self) -> usize {
-        let target_area: TargetArea = (self.x_range.clone(), self.y_range.clone()).into();
-        let (min_x, _) = find_vel_range_x(target_area.min_x, target_area.max_x);
+        let target_area: TargetArea = (self.x_range, self.y_range).into();
         let max_y = -target_area.min_y;
 
         let mut hits = 0;
-        for x in min_x..=target_area.max_x {
+        for x in 0..=target_area.max_x {
+            if sum(x) < target_area.min_x {
+                continue;
+            }
+
             for y in target_area.min_y..max_y {
-                let vel = Point(x, y);
-                if sim(vel, &target_area) {
+                if sim(Point(x, y), &target_area) {
                     hits += 1;
                 }
             }
@@ -65,28 +56,12 @@ impl Runner for AOC17 {
     }
 }
 
+#[inline]
 fn sum(num: isize) -> isize {
     num * (num + 1) / 2
 }
 
-fn find_vel_range_x(xs: isize, xe: isize) -> (isize, isize) {
-    let mut min = isize::MAX;
-    let mut max = isize::MIN;
-
-    for i in 0..xe {
-        let s = sum(i);
-        if s < xs {
-            continue;
-        } else if s > xe {
-            break;
-        }
-        min = min.min(i);
-        max = max.max(i);
-    }
-
-    (min, max)
-}
-
+#[inline]
 fn sim(mut vel: Point, target: &TargetArea) -> bool {
     let mut pos = Point(0, 0);
     loop {
@@ -106,13 +81,12 @@ fn sim(mut vel: Point, target: &TargetArea) -> bool {
     false
 }
 
+#[inline]
 fn dir(num: isize) -> isize {
-    if num == 0 {
-        0
-    } else if num < 0 {
-        -1
-    } else {
-        1
+    match num {
+        0 => 0,
+        _ if num < 0 => -1,
+        _ => 1,
     }
 }
 
@@ -124,30 +98,19 @@ struct TargetArea {
     max_y: isize,
 }
 
-impl From<(RangeInclusive<isize>, RangeInclusive<isize>)> for TargetArea {
-    fn from(ranges: (RangeInclusive<isize>, RangeInclusive<isize>)) -> Self {
-        let mut mmx = Point(isize::MAX, isize::MIN);
-        let mut mmy = Point(isize::MAX, isize::MIN);
-
-        for x in ranges.0 {
-            for y in ranges.1.clone() {
-                mmx.0 = mmx.0.min(x);
-                mmx.1 = mmx.1.max(x);
-                mmy.0 = mmy.0.min(y);
-                mmy.1 = mmy.1.max(y);
-            }
-        }
-
+impl From<((isize, isize), (isize, isize))> for TargetArea {
+    fn from(ranges: ((isize, isize), (isize, isize))) -> Self {
         Self {
-            min_x: mmx.0,
-            min_y: mmy.0,
-            max_x: mmx.1,
-            max_y: mmy.1,
+            min_x: ranges.0 .0,
+            min_y: ranges.1 .0,
+            max_x: ranges.0 .1,
+            max_y: ranges.1 .1,
         }
     }
 }
 
 impl TargetArea {
+    #[inline]
     fn contains(&self, point: Point) -> bool {
         point.0 >= self.min_x
             && point.0 <= self.max_x
@@ -155,6 +118,7 @@ impl TargetArea {
             && point.1 <= self.max_y
     }
 
+    #[inline]
     fn passed(&self, point: Point) -> bool {
         point.0 > self.max_x || point.1 < self.min_y
     }
